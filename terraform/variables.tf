0d153dbeb1ba805420698827a65c389ed00b2a1c
KomarovAI <artur.komarovv@gmail.com>
2025-10-24 00:31:48 +0300

    chore(tf): add variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "vps_host" { type = string }
+variable "vps_user" { type = string default = "root" }
+variable "ssh_private_key" { type = string sensitive = true }
+variable "domain_name" { type = string }
+variable "admin_email" { type = string }
+variable "cockpit_password" { type = string sensitive = true }
+variable "marzban_username" { type = string default = "admin" }
+variable "marzban_password" { type = string sensitive = true }
+variable "dnsexit_api_key" { type = string sensitive = true }
