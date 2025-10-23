9e71dfe85b99e9cc7f3ca257cfe400ded61ff1da
KomarovAI <artur.komarovv@gmail.com>
2025-10-24 02:02:17 +0300

    ♾️ Refactor variables.tf - remove unused vars, add descriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,46 +1,44 @@
 variable "vps_host" {
-  type = string
+  type        = string
+  description = "VPS server IP address or hostname"
 }
 
 variable "vps_user" {
-  type    = string
-  default = "root"
+  type        = string
+  default     = "root"
+  description = "SSH user for VPS connection"
 }
 
 variable "ssh_private_key" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "SSH private key for VPS authentication"
 }
 
 variable "domain_name" {
-  type = string
+  type        = string
+  description = "Primary domain name for the server"
 }
 
 variable "admin_email" {
-  type = string
+  type        = string
+  description = "Administrator email for SSL certificates and notifications"
 }
 
 variable "cockpit_password" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "Password for Cockpit web interface admin user"
 }
 
 variable "marzban_username" {
-  type    = string
-  default = "admin"
+  type        = string
+  default     = "admin"
+  description = "Marzban VPN panel admin username"
 }
 
 variable "marzban_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "dnsexit_api_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "root_ip" {
-  type    = string
-  default = "31.59.58.96"
+  type        = string
+  sensitive   = true
+  description = "Marzban VPN panel admin password"
 }
