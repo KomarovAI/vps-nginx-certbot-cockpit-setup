4a15eb8d208449ab0e6c6d2334a233f38062bf76
KomarovAI <artur.komarovv@gmail.com>
2025-10-24 01:08:57 +0300

    fix(tf): rewrite variables.tf to proper multiline blocks and add root_ip variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,46 @@
-variable "vps_host" { type = string }
-variable "vps_user" { type = string default = "root" }
-variable "ssh_private_key" { type = string sensitive = true }
-variable "domain_name" { type = string }
-variable "admin_email" { type = string }
-variable "cockpit_password" { type = string sensitive = true }
-variable "marzban_username" { type = string default = "admin" }
-variable "marzban_password" { type = string sensitive = true }
-variable "dnsexit_api_key" { type = string sensitive = true }
+variable "vps_host" {
+  type = string
+}
+
+variable "vps_user" {
+  type    = string
+  default = "root"
+}
+
+variable "ssh_private_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "admin_email" {
+  type = string
+}
+
+variable "cockpit_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "marzban_username" {
+  type    = string
+  default = "admin"
+}
+
+variable "marzban_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "dnsexit_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "root_ip" {
+  type    = string
+  default = "31.59.58.96"
+}
